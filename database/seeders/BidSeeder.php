801b4490b2bb29049202eb1fff2390b235fc28d8
Hezekiah Logronio <hezekiahlogronio@Hezekiahs-MacBook-Air.local>
2025-10-13 00:00:25 +0800

    Fix BidSeeder to match actual database schema

--- database/seeders/BidSeeder.php
+++ database/seeders/BidSeeder.php
@@ -10,9 +10,29 @@ class BidSeeder extends Seeder
 {
     public function run()
     {
-        Bid::create(['title' => 'Office Supplies', 'type' => 'alternative']);
-        Bid::create(['title' => 'Equipment Repair', 'type' => 'alternative']);
-        Bid::create(['title' => 'Computer Units', 'type' => 'competitive']);
-        Bid::create(['title' => 'Internet Service', 'type' => 'competitive']);
+        // Create sample bids with the correct schema fields
+        Bid::create([
+            'purchase_request_id' => 1,
+            'vendor_name' => 'Office Supplies Vendor',
+            'amount' => 5000.00
+        ]);
+        
+        Bid::create([
+            'purchase_request_id' => 1,
+            'vendor_name' => 'Equipment Repair Vendor',
+            'amount' => 3500.00
+        ]);
+        
+        Bid::create([
+            'purchase_request_id' => 2,
+            'vendor_name' => 'Computer Units Vendor',
+            'amount' => 25000.00
+        ]);
+        
+        Bid::create([
+            'purchase_request_id' => 2,
+            'vendor_name' => 'Internet Service Provider',
+            'amount' => 12000.00
+        ]);
     }
 }
