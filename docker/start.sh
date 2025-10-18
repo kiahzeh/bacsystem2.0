39041de457afdc43922e66ecb4bd6ebea91d34c4
Hezekiah Logronio <hezekiahlogronio@Hezekiahs-MacBook-Air.local>
2025-10-18 21:18:41 +0800

    chore(apache): silence AH00558 by enabling ServerName localhost at runtime

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,8 @@ PORT="${PORT:-8080}"
 
 # Ensure Apache listens on the provided PORT
 printf "Listen %s\n" "$PORT" > /etc/apache2/ports.conf
+printf "ServerName localhost\n" > /etc/apache2/conf-available/servername.conf
+ a2enconf servername
 
 # Align the default vhost to the runtime PORT (from 80)
 sed -i "s#<VirtualHost \*:[0-9]\+#<VirtualHost *:${PORT}#" /etc/apache2/sites-available/000-default.conf
