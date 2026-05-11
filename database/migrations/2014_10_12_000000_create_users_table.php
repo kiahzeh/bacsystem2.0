1d9626bc16b3d2175d73a4937baa9f2f77c30c26
Hezekiah Logronio <hezekiahlogronio@Hezekiahs-MacBook-Air.local>
2026-05-11 13:46:30 +0800

    Fix users table schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,15 +16,23 @@ return new class extends Migration
             return;
         }
 
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+       Schema::create('users', function (Blueprint $table) {
+    $table->id();
+    $table->string('name');
+    $table->string('email')->unique();
+    $table->timestamp('email_verified_at')->nullable();
+    $table->string('password');
+
+    $table->foreignId('department_id')->nullable()->constrained()->nullOnDelete();
+
+    $table->boolean('is_approved')->default(false);
+    $table->boolean('is_admin')->default(false);
+
+    $table->string('role')->default('user');
+
+    $table->rememberToken();
+    $table->timestamps();
+});
     }
 
     /**
